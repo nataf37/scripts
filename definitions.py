ac0ec4578798818a65a30993dc81ba004ea7c7fc
Sasha Smolyak <ssmolyak@redhat.com>
2018-04-30 14:02:58 +0300

    fix in image name

--- definitions.py
+++ definitions.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 image_source = "http://ikook.tlv.redhat.com/gen_images/cloud/CentOS-7-x86_64-GenericCloud.qcow2"
-image_name = "CentOS-7-x86_64-GenericCloud-1509.qcow2"
+image_name = "CentOS-7-x86_64-GenericCloud.qcow2"
 pipeline_file = "/etc/ceilometer/pipeline.yaml"
 gnocchi_resources_file = "/etc/ceilometer/gnocchi_resources.yaml"
 docker_file = "docker exec -ti"
