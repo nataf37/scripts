be1abf69dba3e1978ac53c7d86d5c05d455529cd
nsharabi <nsharabi@redhat.com>
2019-06-03 16:10:09 +0300

    Added backup commands

--- BackupNRestore.sh
+++ BackupNRestore.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-Titan=Titan41.lab.eng.tlv2.redhat.com
+#This script should be run under root user
+#This script should be run from the hypervisor
 
 def login()
 
-def backup()
+
+
+def backup(){
+
+ssh undercloud-0 " 
+yum install mariadb;
+cp /var/lib/config-data/puppet-generated/mysql/root/.my.cnf .;
+mkdir /backup;
+chown stack: /backup;
+mysqldump --opt --all-databases > /root/undercloud-all-databases.sql;
+cd /backup;
+tar --xattrs --ignore-failed-read -vcf \
+    undercloud-backup-`date +%F`.tar \
+    /etc \
+    /var/log \
+    /var/lib/glance \
+    /var/lib/certmonger \
+    /var/lib/registry \
+    /var/lib/config-data \
+    /srv/node \
+    /root \
+    /home/stack;
+"
+}
 
 def restore()
 
 
 
-main()
 
+def main()
+
+
+main
